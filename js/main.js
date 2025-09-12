ef4ee9ab2611b7307e5a27ae3d854e39d1b11e4b
Fumiaki Arai <fumiaki.arai.6261@gmail.com>
2025-09-12 16:18:32 +0900

    スムーススクロール

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ document.querySelector('.pagetop').addEventListener('click', function(e) {
       behavior: 'smooth' // スムーズスクロール
     });
   });
+$(function(){
+  // .header_link がクリックされたとき
+  $('a.header_link').click(function(e){
+    e.preventDefault(); // デフォルトのジャンプ動作を止める
+
+    var target = $(this).attr('href'); // クリックしたリンクのhref取得
+    var position = $(target).offset().top; // 対象の位置
+
+    $('html, body').animate({scrollTop: position}, 600, 'swing'); 
+    // 600msかけてスムーズにスクロール
+  });
+});
