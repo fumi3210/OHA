871e94866bd27c0b6d9adc14ab59edc0697e5fa6
Fumiaki Arai <fumiaki.arai.6261@gmail.com>
2025-09-13 02:57:02 +0900

    モーダル

--- js/main.js
+++ js/main.js
@@ -17,3 +17,20 @@ $(function(){
     // 600msかけてスムーズにスクロール
   });
 });
+
+
+const intersectionObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
+    } else {
+      entry.target.classList.remove("is-in-view");
+    }
+  });
+});
+
+// 監視対象を追加
+const inViewItems =  document.querySelectorAll(".js-in-view ");
+inViewItems.forEach(function(inViewItems){
+  intersectionObserver.observe(inViewItems);
+});
