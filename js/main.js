f33aa49cc4a9fc3504211f9b3911dc5faa710bc8
Fumiaki Arai <fumiaki.arai.6261@gmail.com>
2025-09-18 17:07:55 +0900

    gallery

--- js/main.js
+++ js/main.js
@@ -76,7 +76,27 @@ $(".drawer_content-link").on("click", function() {
       });
     });
   });
+// swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 
 
